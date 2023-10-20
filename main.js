ce68a0b1608469cd7f460001f9d102d8c3181822
MARIANA ALVES DE SOUSA <23111755@CFP127ALUNO.LOCAL>
2023-10-20 10:45:39 -0300

    finalizado

---
+++ main.js
@@ -0,0 +1,15 @@
+/***********************************************************
+* Objetivo: Trazer informaçôes sobre estados do Brasil
+* Data: 20/10/2023
+* Autor:Mariana Alves
+* Versão: 1.0.0
+************************************************************/
+
+var estadoCidades = require ('./modulo/estados_cidades.js')
+
+const getListaDeEsados = function(){
+
+
+    let 
+}
+console.log('lkhgS');
